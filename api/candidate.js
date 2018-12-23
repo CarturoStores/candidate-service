e63d07a24e4119dcbc4c51f81314cfa4bbaaaae0
David Perez <kitsunebrave@gmail.com>
2018-12-23 13:33:40 -0500

    get one candidate

--- api/candidate.js
+++ api/candidate.js
@@ -40,6 +40,29 @@ module.exports.submit = (event, context, callback) => {
     });
 };
 
+module.exports.get = (event, context, callback) => {
+  const params = {
+    TableName: process.env.CANDIDATE_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  dynamoDb.get(params).promise()
+    .then(result => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result.Item),
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch candidate.'));
+      return;
+    });
+};
+
 module.exports.list = (event, context, callback) => {
   var params = {
       TableName: process.env.CANDIDATE_TABLE,
@@ -66,6 +89,29 @@ module.exports.list = (event, context, callback) => {
 
   dynamoDb.scan(params, onScan);
 
+};
+
+module.exports.get = (event, context, callback) => {
+  const params = {
+    TableName: process.env.CANDIDATE_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  dynamoDb.get(params).promise()
+    .then(result => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result.Item),
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch candidate.'));
+      return;
+    });
 };
 
 const submitCandidateP = candidate => {
